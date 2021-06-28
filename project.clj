ce54991941fdd282846396ecd69ab36b94f8cbf0
Max Penet <max.penet@exoscale.ch>
2021-06-28 16:32:18 +0200

    Version 0.1.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/xi "0.1.5"
+(defproject cc.qbits/xi "0.1.6-SNAPSHOT"
   :description ""
   :url "http://github.com/mpenet/xi"
   :license {:url "https://www.eclipse.org/legal/epl-2.0/"}
