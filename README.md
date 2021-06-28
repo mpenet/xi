10ac88a53eb9afcb60b3527187711d77faf52640
Max Penet <max.penet@exoscale.ch>
2021-06-28 14:18:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# xi Ξ
+
+Protocol abstracting async calls
+
+## Documentation
+
+## Installation
+
+xi is [available on Clojars](https://clojars.org/mpenet/xi).
+
+Add this to your dependencies:
+
+[![Clojars Project](https://img.shields.io/clojars/v/mpenet/xi.svg)](https://clojars.org/mpenet/xi)
+
+## License
+
+Copyright © 2021 [Max Penet](https://s-exp.com)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject xi "0.1.0-SNAPSHOT"
+  :description ""
+  :url "http://github.com/mpenet/xi"
+  :license {:url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.2"]]
+  :cljfmt {:remove-multiple-non-indenting-spaces? true}
+  :global-vars {*warn-on-reflection* true}
+  :deploy-repositories [["snapshots" :clojars] ["releases" :clojars]])
